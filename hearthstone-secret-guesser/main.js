94c67cc71ecda2ad376dd5f3375112c085d7ef1c
Andrew Groff <andyg@rescuescg.com>
2016-11-29 19:45:42 -0800

    Adding remaining basic functionality

--- hearthstone-secret-guesser/main.js
+++ hearthstone-secret-guesser/main.js
@@ -2,31 +2,49 @@
 
     var cardList = [];
     var triggers = {};
+    var triggered = {};
     var selectedOpponent = '';
 
     var buildTriggerList = function(){
-        $.each(cardList, function(i, item){
-            var trigger = item.trigger;
-
-            if(trigger != ''){
-                triggers[trigger] = {
-                    activated : false,
-                    available : true,
-                    score : 0
-                };
-            }
-        });
+//        $.each(cardList, function(i, item){
+//            var trigger = item.trigger;
+//
+//            if(trigger != ''){
+//                triggers[trigger] = {
+//                    activated : false,
+//                    available : true,
+//                    score : 0
+//                };
+//            }
+//        });
     };
 
     var renderTriggers = function(){
-        var $container = $("#triggers-container");
+        var $container = $("#triggers-container"),
+            $usedContainer = $("#triggered-container"),
+            keysSorted = Object.keys(triggers).sort(function(a,b){
+                return triggers[b]-triggers[a]
+            });
         $container.html("");
-        $.each(triggers, function(triggerName, triggerInfo){
-            var cssClass = 'btn-default';
-            if(triggerInfo.activated === true){
-                cssClass = 'btn-success'
+        $usedContainer.html("");
+        $.each(keysSorted, function(i, triggerName) {
+            var cssClass = 'btn-primary',
+                triggerValue = triggers[triggerName],
+                button = '<button type="button" data-trigger="'
+                    +triggerName
+                    +'" class="btn '
+                    +cssClass
+                    +'">'
+                    +triggerName+' ('+triggerValue
+                    +')</button>';
+
+            if(triggered.hasOwnProperty(triggerName)){
+                $usedContainer.append(button);
             }
-            $container.append('<button type="button" class="btn '+cssClass+'">'+triggerName+' ('+triggerInfo.score+')</button>');
+            else {
+                $container.append(button);
+            }
+
         });
     };
 
@@ -44,37 +62,78 @@
 
     var renderCards = function(){
         var $available = $("#available-cards"),
+            isStandard = $("#mode-select").val() === 'standard',
             newCardList;
 
         $available.html("");
+        triggers = {};
         $.each(cardList, function(i, item){
+            var trigger = item.trigger;
             if(item.class !== selectedOpponent){
                 return;
             }
+            if(isStandard && item.wild === true){
+                return;
+            }
 
-            $available.append(getCardMarkup(item));
+            if(!triggers.hasOwnProperty(trigger)){
+                triggers[trigger] = 0;
+            }
+            triggers[trigger] += Math.floor(item.usage);
+
+            if(!triggered.hasOwnProperty(trigger)){
+                $available.append(getCardMarkup(item));
+            }
+
         });
+
+        console.log(triggers);
+
+        renderTriggers();
+    };
+
+    var reset = function(){
+        triggered = {};
+        renderCards();
     };
 
     var doBindings = function(){
 
         var classChanged = function(val){
-            var $interface = $("#unavailable-cards, #available-cards, #trigger-interface");
+            var $interface = $("#unavailable-cards, #available-cards, #trigger-interface, #mode-select");
 
             if(val === ''){
                 $interface.hide();
+                reset();
             }
             else {
                 $interface.show();
                 selectedOpponent = val;
-                renderCards();
+                reset();
             }
+
         };
 
         $("#class-select").change(function(){
             classChanged($(this).val())
         });
         classChanged('');
+
+        $("#triggers-container").on('click', 'button', function(){
+            triggered[$(this).attr('data-trigger')] = 1;
+            renderCards();
+        });
+        $("#triggered-container").on('click', 'button', function(){
+            delete triggered[$(this).attr('data-trigger')];
+            renderCards();
+        });
+
+        $("#mode-select").change(function(){
+            renderCards();
+        });
+        $("#resetButton").click(function(){
+            reset();
+        });
     };
 
     var initialize = function(){
@@ -93,7 +152,6 @@
 
             buildTriggerList();
             renderCards();
-            renderTriggers();
             doBindings();
         });
     };
